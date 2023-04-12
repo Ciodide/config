633e7bed4a1111c305f4ea839e1c2e0e2df0fc9b
Ciodide Bako <ciodide@iite.st>
2023-04-12 12:36:45 +0800

    Xmonad: Add fullscreen support, and gap between windows.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,7 +1,21 @@
-import XMonad
+import           XMonad
+import           XMonad.Util.EZConfig
+import           XMonad.Util.Ungrab
+import           XMonad.Hooks.EwmhDesktops
+import           XMonad.Layout.Spacing
+import XMonad.Hooks.ManageDocks
 
-main = xmonad def
-    { terminal    = "alacritty"
-    , modMask     = mod4Mask
-    , borderWidth = 0
-    }
+main = xmonad
+  $ ewmhFullscreen
+  $ ewmh
+  $ docks
+  $ def { terminal = "alacritty"
+        , modMask = mod4Mask
+        , borderWidth = 0
+        , layoutHook =
+            avoidStruts
+            $ smartSpacingWithEdge 7 $ Tall 1 (3 / 100) (1 / 2)
+        }
+  `additionalKeys` [ ((mod4Mask, xK_r), spawn "dmenu_run -fn Spleen-22")
+                   , ((mod4Mask .|. shiftMask, xK_l), spawn "xsecurelock")
+                   , ((mod4Mask, xK_Return), spawn "alacritty")]
